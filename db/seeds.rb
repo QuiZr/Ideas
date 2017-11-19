dfe34781c98186c0af7bd70de7a90930358fdb65
kamil506 <kamil.debicki15@wp.pl>
2017-11-19 05:52:57 +0100

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,8 +20,19 @@ end
 
 5.times do |i|
   @idea = Idea.create(title: Faker::Book.title, desc_short: Faker::Lorem.sentence, desc_long: Faker::Lorem.paragraph, user_id: i+1, status: 0)
-  Comment.create(idea_id: @idea.id, body: Faker::Lorem.sentence, user_id: i+1)
+  10.times do
+    Comment.create(idea_id: @idea.id, body: Faker::Lorem.sentence, user_id: Faker::Number.between(User.first.id, User.last.id))
+  end
   3.times do
     @idea.tags << Tag.find(Faker::Number.between(Tag.first.id, Tag.last.id))
   end
+end
+
+20.times do
+  Idea.create(title: Faker::Book.title,
+              desc_short: Faker::Lorem.sentence,
+              desc_long: Faker::Lorem.paragraph,
+              user_id: Faker::Number.between(User.first.id, User.last.id),
+              status: Faker::Number.between(0,3)
+  )
 end
